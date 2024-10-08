54b0410c2ed72c75397cac8c77538d10aca2723b
Ai Onaga <a.onaga@shunju.io>
2024-10-08 11:52:03 +0900

    Improve the README

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "hello#index"
-  get "hello" => "hello#index"
+  root "application#hello"
 end
